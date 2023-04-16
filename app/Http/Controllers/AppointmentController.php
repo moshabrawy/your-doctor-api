c7d9bab17eaed0c60870a4d9c7439c1261ee6d86
moshabrawy <mo.shabrawydev@gmail.com>
2023-04-17 01:52:46 +0200

    booking 2st

--- app/Http/Controllers/AppointmentController.php
+++ app/Http/Controllers/AppointmentController.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers;
 use App\Http\Resources\AppointmentResource;
 use App\Models\Appointment;
 use App\Models\PatientDetail;
+use App\Models\TimeSlot;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class AppointmentController extends Controller
 {
@@ -23,12 +26,19 @@ class AppointmentController extends Controller
         $data = AppointmentResource::collection($appointments);
         return response()->json(['data' => $data, 'status_code' => 200]);
     }
+
     public function booking(Request $request)
     {
         $validation = Validator::make($request->all(), [
-            'slot_id' => 'required',
-            'doctor_id' => 'required',
-            'day_date' => 'required',
+            'slot_id' => [
+                'required',
+                Rule::exists('time_slots', 'id'),
+            ],
+            'doctor_id' => [
+                'required',
+                Rule::exists('users', 'id'),
+            ],
+            'booking_date' => 'required',
             'patient_name' => 'required',
             'age' => 'required',
             'disease_dec' => 'required',
@@ -36,11 +46,16 @@ class AppointmentController extends Controller
         if ($validation->fails()) {
             return response()->json(['error' => $validation->errors(), 'status_code' => 400]);
         } else {
+            $slot = TimeSlot::find($request->slot_id);
+            $booking_date = Carbon::parse($request->booking_date)->format('l');
+            if ($booking_date != $slot->day_en) {
+                return response()->json(['error' => 'Wrong Booking Date', 'status_code' => 400]);
+            }
             $appointment = Appointment::create([
                 'slot_id' => $request->slot_id,
                 'user_id' => auth()->user()->id,
                 'doctor_id' => $request->doctor_id,
-                'day_date' => $request->day_date
+                'day_date' => $request->booking_date
             ]);
             PatientDetail::create([
                 'appointment_id' => $appointment->id,
