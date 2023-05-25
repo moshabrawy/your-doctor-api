bb107ea892df8e33026a9eaa6de8c73f171bb970
Mo Shabrawy <mohamedshabrawy@affsquare.com>
2023-05-25 14:23:47 +0300

    specialty

--- app/Http/Controllers/Dashboard/SpecialtyController.php
+++ app/Http/Controllers/Dashboard/SpecialtyController.php
@@ -47,35 +47,52 @@ class SpecialtyController extends Controller
         return redirect()->route('specialties.index');
     }
 
-
     public function edit(Specialty $specialty)
     {
         return view('dashboard.specialties.edit', compact('specialty'));
     }
 
-    public function update(Request $request, Specialty $specialty)
+    public function update(Request $request, $id)
     {
-        $request->validate([
-            'name' => 'required'
-        ]);
-        $input = $request->except('_token', '_method');
-        $specialty->spec_icon = $input['spec_icon'];
-        $specialty->about = $input['about'];
-        $specialty->update($input);
-        return Redirect::route('specialties.edit', $specialty->id)->with('success', 'Done');
+        $specialty = Specialty::where('id', $id)->first();
+        if ($specialty) {
+            $request->validate([
+                "title" => "sometimes|required|unique:specialties,title," . $specialty->id,
+                "brief" => "sometimes|required|max:500",
+            ]);
+
+            $specialty->title = $request->title ?? $specialty->title;
+            if ($request->file('image')) {
+                $request->validate(["image" => "sometimes|image|mimes:jpeg,png,jpg|max:5128"]);
+                $specialty->image = $this->UploudAvatar($request->file('image'), 'specialities') ?? $specialty->image;
+            }
+            $specialty->brief = $request->brief ?? $specialty->brief;
+            $specialty->save();
+
+            notify()->success('You are awesome, Specialty has been Updated successfull!');
+            return redirect()->route('specialties.index');
+        } else {
+            notify()->success('Opps, Update Faild!');
+            return redirect()->back();
+        }
     }
 
     public function search(Request $request)
     {
         $search = $request->get('search');
         $allSpecialties = Specialty::where('name', 'like', '%' . $search . '%')->paginate(10);
-        return view('dashboard.specialties.all', compact('allSpecialties'))->with('SearchDone', 'Search Done');
+        return view('dashboard.specialties.manage', compact('allSpecialties'));
     }
 
-    public function destroy($id)
+    public function destroy(string $id)
     {
-        $specialty = Specialty::findOrFail($id);
-        $specialty->delete();
-        return Redirect::route('specialties.index')->with('success', 'Done');
+        $specialty = Specialty::find($id);
+        if ($specialty) {
+            $specialty->delete();
+            notify()->success('You are awesome, The Specialty has been deleted successfull!');
+        } else {
+            notify()->error('Opps!, The Specialty has been deleted before');
+        }
+        return redirect()->back();
     }
 }
--- resources/views/dashboard/specialties/edit.blade.php
+++ resources/views/dashboard/specialties/edit.blade.php
@@ -1,101 +1,85 @@
 @extends('dashboard.layout')
 
 @section('content')
-    <div class="page-header">
-        <h3 class="page-title">
-            <span class="page-title-icon bg-gradient-primary text-white mr-2">
-                <i class="mdi mdi-bookmark-outline"></i>
-            </span>
-            Edit Specialty
-        </h3>
-        <nav aria-label="breadcrumb">
-            <ul class="breadcrumb">
-                <li class="breadcrumb-item active" aria-current="page">
-                    <span></span>Overview <i class="mdi mdi-alert-circle-outline icon-sm text-primary align-middle"></i>
-                </li>
-            </ul>
-        </nav>
-    </div>
-    @auth
-        <div class="row">
-            <div class="col-md-4 grid-margin stretch-card">
-                <div class="card">
-                    <div class="card-body text-center">
-                        <div class="avatar">
-                            @if ($specialty->spec_icon != null)
-                                <div class="nav_specialty_icon">
-                                    <i class="{{ $specialty->spec_icon }} "></i>
-                                </div>
-                            @else
-                                <div class="nav_specialty_icon">
-                                    <i class="mdi mdi-apps"></i>
-                                </div>
-                            @endif
-                        </div><br>
-                        <h3 class="specialty_name badge badge-gradient-primary">{{ $specialty->name }}</h3>
-                        <div class="about text-left">
-                            <h4 class="py-2 text-info"> <i class="mdi mdi-information-outline "></i> About</h4>
-                            <p> {{ $specialty->about }} </p>
-                        </div>
+<div class="page-header">
+    <h3 class="page-title">
+        <span class="page-title-icon bg-gradient-primary text-white mr-2">
+            <i class="mdi mdi-bookmark-outline"></i>
+        </span>
+        Edit Specialty
+    </h3>
+</div>
+@auth('admin')
+<div class="row">
+    <div class="col-md-4 grid-margin stretch-card">
+        <div class="card">
+            <div class="card-body text-center">
+                <div class="avatar">
+                    <div class="nav_specialty_icon">
+                        <img src="{{$specialty->image}}" alt="{{$specialty->title}}">
                     </div>
-                </div>
-            </div>
-            <div class="col-md-8 grid-margin stretch-card">
-                <div class="card">
-                    @if (session()->has('success'))
-                        <div class="card card-inverse-success" id="context-menu-open">
-                            <div class="card-body">
-                                <p class="card-text"> Greate! Update Successfully</p>
-                            </div>
-                        </div>
-                    @elseif (session()->has('error'))
-                        <div class="card card-inverse-danger" id="context-menu-open">
-                            <div class="card-body">
-                                <p class="card-text"> oops! Update Fail</p>
-                            </div>
-                        </div>
-                    @endif
-                    <div class="card-body">
-                        <h4 class="card-title">Edit
-                            <label class="badge badge-gradient-primary">{{ ' ' . $specialty->name . ' ' }}</label>
-                            Information
-                        </h4><br>
-                        <form method="POST" action="{{ route('specialties.update', $specialty->id) }}" >
-                            @csrf
-                            @method('patch')
-                            <div class="form-group row">
-                                <label for="pat_name" class="col-sm-3 col-form-label">Name</label>
-                                <div class="col-sm-9">
-                                    <input type="text" class="form-control" id="name" name="name"
-                                        placeholder="ex:- Dentistry" value="{{ $specialty->name }}"><br>
-                                    @error('name')
-                                        <div class="alert alert-danger"> {{ $message }}</div>
-                                    @enderror
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="spec_icon" class="col-sm-3 col-form-label">Specialty Icon Class</label>
-                                <div class="col-sm-9">
-                                    <input type="text" class="form-control" id="spec_icon" name="spec_icon"
-                                        placeholder="ex:- mdi mdi-account" value="{{ $specialty->spec_icon }}"><br>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="about" class="col-sm-3 col-form-label">About Specialty</label>
-                                <div class="col-sm-9">
-                                    <textarea name="about" id="about" rows="10" class="form-control">
-                                        {{ $specialty->about }}
-                                    </textarea>
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <button type="submit" class="btn btn-gradient-primary mr-2">Update</button>
-                                <button class="btn btn-light">Cancel</button>
-                            </div>
-                        </form>
-                    </div>
+                </div><br>
+                <h3 class="specialty_name badge badge-gradient-primary">{{ $specialty->title }}</h3>
+                <div class="about text-left">
+                    <h4 class="py-2 text-info"> <i class="mdi mdi-information-outline "></i> Brief</h4>
+                    <p> {{ $specialty->brief }} </p>
                 </div>
             </div>
         </div>
-    @endauth
+    </div>
+    <div class="col-md-8 grid-margin stretch-card">
+        <div class="card">
+            <div class="card-body">
+                <h4 class="card-title">Edit
+                    <label class="badge badge-gradient-primary">{{ ' ' . $specialty->title . ' ' }}</label>
+                    Information
+                </h4><br>
+                <form method="POST" action="{{ route('specialties.update', $specialty->id) }}">
+                    @csrf
+                    @method('patch')
+                    <div class="form-group row">
+                        <label for="title" class="col-sm-3 col-form-label">Specialty Title</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" id="title" name="title" placeholder="ex:- Dentistry" value="{{ $specialty->title }}">
+                            @error('title')
+                            <div class="alert alert-danger mt-2 mb-1">
+                                {{ $message  }}
+                            </div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="image" class="col-sm-3 col-form-label">Specialty Photo</label>
+                        <div class="col-sm-9">
+                            <input type="file" class="form-control" id="image" name="image" placeholder="ex:- mdi mdi-account">
+                            @error('image')
+                            <div class="alert alert-danger mt-2 mb-1">
+                                {{ $message  }}
+                            </div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label for="brief" class="col-sm-3 col-form-label">Specialty Brief</label>
+                        <div class="col-sm-9">
+                            <textarea name="brief" id="brief" rows="10" class="form-control">
+                            {{ $specialty->brief }}
+                            </textarea>
+                            @error('brief')
+                            <div class="alert alert-danger mt-2 mb-1">
+                                {{ $message  }}
+                            </div>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <button type="submit" class="btn btn-gradient-primary mr-2">Update</button>
+                        <button class="btn btn-light">Cancel</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+@endauth
 @endsection
