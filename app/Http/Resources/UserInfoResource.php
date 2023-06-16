421b911815328f4c116938205f78846dacd7fbe8
moshabrawy <mo.shabrawydev@gmail.com>
2023-06-16 16:11:27 +0200

    Update UserInfoResource.php

--- app/Http/Resources/UserInfoResource.php
+++ app/Http/Resources/UserInfoResource.php
@@ -21,12 +21,12 @@ class UserInfoResource extends JsonResource
             "first_name" => explode(" ", $this->name)[0],
             "last_name" => explode(" ", $this->name, 2)[1] ?? '',
             'email' => $this->email,
-            'user_type' => $this->user_type === "0" ? 'Doctor' : 'User',
+            'user_type' => $this->user_type === "1" ? 'User' : 'Doctor',
             'avatar' => $this->avatar !=  null ? url('uploads/images/profile/' . $this->avatar) : url('assets/images/user.png'),
             'gender' => $this->gender,
             'phone' => $this->phone,
             'birth_date' => Carbon::parse($this->birth_date)->format('Y-m-d'),
-            $this->mergeWhen($this->user_type === "0", function () {
+            $this->mergeWhen($this->user_type !== "1", function () {
                 return ["specialist" =>  $this->doctor_info->specialty->title];
             }),
             $this->mergeWhen($this->address, function () {
